eb8b6e65179484cea5ee2002552a7260517e1462
William Anthony <16523109@std.stei.itb.ac.id>
2025-01-13 15:11:58 +0700

    will done, finished bro

--- src/app/thankyou/page.tsx
+++ src/app/thankyou/page.tsx
@@ -14,18 +14,11 @@ function Thankyou() {
     <main className="flex w-[100vw] h-[100vh]" style={{ background: 'linear-gradient(111.84deg, #DDC28E -1.42%, #77684C 65.2%)', boxShadow: '0px 4px 4px 0px #00000040' }}>
         {/* Bagian lain dari komponen */}
       <div className="Background Hero w-full h-full m-auto overflow-hidden bg-[url('/mainbg.png')] bg-cover bg-center flex">
-        <div className="logincontainer animate-popup m-auto block pt-10 pb-10 w-[500px] md:w-[700px] h-[200px] rounded-3xl transition-all ease-in-out duration-1000">
-          <div className="title leading-tight w-full text-center text-[60pt] font-extrabold bg-clip-text text-transparent bg-gradient-to-b from-[#221D11] via-[#221D11] to-[#A38855] font-Algerian">
-              THANK YOU!!
-          </div>
-{/*          <div className="maincontent h-fit relative top-[-53px] sm:top-[-68px] md:top-[-86px] lg:top-[-103px] block">
-            <div  className="title leading-tight w-full text-center text-[60pt] font-extrabold text-[#F8E5C1] font-Algerian">
-                THANK YOU!!
-            </div>
-          </div> */}
-          <div className="message w-full flex text-[10pt] sm:text-[12pt] md:text-[14pt] lg:text-[16pt] mt-[-30px] sm:mt-[-40px] md:mt-[-50px] lg:mt-[-70px]">
-            <h1 className="pesan m-auto font-bold font-Montserrat text-[#B3403D]">SUARAMU BERHASIL DIREKAM CHAMP!!</h1>
-          </div>
+        <div className="logincontainer animate-popup m-auto block pt-10 pb-10 w-[500px] md:w-[700px] h-[320px] rounded-3xl transition-all ease-in-out duration-1000">
+        <div className="title leading-tight w-full text-center text-[60pt] font-extrabold bg-clip-text text-transparent bg-gradient-to-b from-[#221D11] via-[#221D11] to-[#A38855] font-Algerian flex justify-center items-center h-full">
+          THANK YOU!!
+        </div>
+        <h1 className="title m-auto leading-tight w-full text-center text-transparent bg-clip-text bg-gradient-to-b from-[#221D11] via-[#221D11] to-[#a97d2b]" style={{ fontFamily: 'Abhaya Libre ExtraBold', fontSize: '26px' }}>SUARAMU BERHASIL DIREKAM CHAMP!!</h1>
         </div>
       </div>
       <NavBar data={ChampID}/>
--- src/app/vote/page.tsx
+++ src/app/vote/page.tsx
@@ -113,23 +113,11 @@ function Vote() {
     <main className="flex w-[100vw] h-[100vh]" style={{ background: 'linear-gradient(111.84deg, #DDC28E -1.42%, #77684C 65.2%)', boxShadow: '0px 4px 4px 0px #00000040' }}>
       {/* Bagian lain dari komponen */}
       <div className="Background Hero w-full h-full m-auto overflow-auto bg-[url('/mainbg.png')] bg-cover bg-center flex">
-        <div className={`voteform w-[95%] md:w-[90%] lg:w-[80%] xl:w-[70%] min-h-[80vh] h-fit m-auto mt-[20vh] shadow-2xl block p-5 sm:p-7 md:p-16 pt-10 bg-gradient-to-b from-white to-[#ccedba] via-white ${popupAnimation}`}>
-          <div className="title block w-full text-center text-black font-Montserrat font-bold text-[13pt] sm:text-[18pt]">
-<<<<<<< HEAD
-            <h1 className="text-[20pt] sm:text-[25pt] font-Algerian bg-clip-text text-transparent bg-gradient-to-r from-[#77684C] to-[#DDC28E]">
-              SURAT SUARA
-            </h1>
-            <h1 className="text-[20pt] sm:text-[25pt] font-Algerian bg-clip-text text-transparent bg-gradient-to-r from-[#77684C] to-[#DDC28E]">
-              PEMIRA HME ITB
-            </h1>
-            <h1 className="text-[20pt] sm:text-[25pt] font-Algerian bg-clip-text text-transparent bg-gradient-to-r from-[#77684C] to-[#DDC28E]">
-              KETUA HIMPUNAN HME ITB 2024/2025
-            </h1>
-=======
+        <div className={`voteform w-[95%] md:w-[90%] lg:w-[80%] xl:w-[70%] min-h-[80vh] h-fit m-auto mt-[20vh] shadow-2xl block p-5 sm:p-7 md:p-16 pt-10 bg-gradient-to-b from-white to-[#ffffff00] via-white ${popupAnimation}`}>
+          <div className="title block w-full text-center text-black font-Algerian font-bold text-[14pt] sm:text-[20pt]">
             <h1 className="">SURAT SUARA</h1>
             <h1 className="">PEMIRA HME ITB</h1>
             <h1 className="">KETUA HIMPUNAN HME ITB 2024/2025</h1>
->>>>>>> 8719500d5e4e3b009f10221ad883d407f4b83b21
           </div>
           <div className="voteform block sm:flex w-full h-fit pt-5">
             <div className="kolomkiri w-full sm:w-[50%] h-fit flex p-8">
@@ -141,7 +129,7 @@ function Vote() {
                 </div>
                 <div className="gambar w-full h-[70%] border-solid border-[1.5px] border-black bg-[url('/poggy.png')] bg-cover bg-center"></div>
                 <div className={`nama ${styleCard1} w-full h-[15%] border-solid border-[1.5px] border-black flex`}>
-                  <h2 className="nama m-auto text-[15pt] font-Algerian">Poggy M. Gultom</h2>
+                  <h2 className="nama m-auto text-[16pt] font-Algerian">Poggy M. Gultom</h2>
                 </div>
               </div>
             </div>
@@ -154,18 +142,19 @@ function Vote() {
                 </div>
                 <div className="gambar w-full h-[70%] border-solid border-[1.5px] border-black bg-[url('/kotak.png')] bg-cover bg-center"></div>
                 <div className={`nama ${styleCard2} w-full h-[15%] border-solid border-[1.5px] border-black flex`}>
-                  <h2 className="nama  m-auto text-[15pt] font-Montserrat">Kotak Kosong</h2>
+                  <h2 className="nama  m-auto text-[16pt] font-Algerian">Kotak Kosong</h2>
                 </div>
               </div>
             </div>
           </div>
           <div className="buttoncontainer w-full h-fit flex pt-5">
-              <button 
-                className={`buttonsubmit text-white p-2 pl-5 pr-5 font-bold ${styleSubmit} rounded-full m-auto text-[16pt] transition-all ease-in-out duration-300`}
+                <button 
+                className={`buttonsubmit text-white p-2 pl-5 pr-5 font-Algerian ${styleSubmit} rounded-full m-auto text-[16pt] transition-all ease-in-out duration-300`}
                 onClick={() => {verification(isAbleToSubmit)}}
+                style={{ backgroundColor: '#FFC045' }}
                 >
                   SUBMIT
-              </button>
+                </button>
           </div>
         </div>
       </div>
